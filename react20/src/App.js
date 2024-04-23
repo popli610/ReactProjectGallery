6a9390607f523e789c10886e429a9a0b4614b7c0
Nishant Popli <nishantpopli1234@gmail.com>
2024-04-24 01:52:19 +0530

    React 20

--- react20/src/App.js
+++ react20/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Accordian from "./components/Accordian";
+import HexColor from "./components/HexColor";
 
 function App() {
   return (
     <div className="App">
       <Accordian/>
+      <HexColor/>
     </div>
   );
 }
---
+++ react20/src/components/HexColor/index.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+const HexColor = () => {
+  const [typeColor, setTypeColor] = useState("hex");
+  const [color, setColor] = useState("");
+  const all = "ABCDEFabcdef0123456789";
+  const randomHexNumber = () => {
+    return all.charAt(Math.floor(Math.random() * all.length));
+  };
+  useEffect(()=>{createRandomColor()}, [typeColor]);
+  const createRandomColor = () => {
+    if (typeColor === "rgb") {
+      setColor(
+        `rgb(${Math.floor(Math.random() * 255)},${Math.floor(
+          Math.random() * 255
+        )},${Math.floor(Math.random() * 255)})`
+      );
+    } else if (typeColor === "hex") {
+      setColor(
+        `#${randomHexNumber()}${randomHexNumber()}${randomHexNumber()}${randomHexNumber()}${randomHexNumber()}${randomHexNumber()}`
+      );
+    }
+  };
+
+  const createHex = () => {
+    setTypeColor("hex");
+  };
+  const createRgb = () => {
+    setTypeColor("rgb");
+  };
+  return (
+    <div className="container" style={{ backgroundColor: color ,height:'100vh', width:'100vw', display:"flex",flexDirection:"column", justifyContent:"space-evenly", alignItems:"center"}}>
+      <div className="buttons">
+        <button className="" onClick={createHex}>
+          Create HEX Color
+        </button>
+        <button className="" onClick={createRgb}>
+          Create RGB Color
+        </button>
+        <button className="" onClick={createRandomColor}>
+          Generate Random Color
+        </button>
+      </div>
+      <div className="content">{color}</div>
+    </div>
+  );
+};
+
+export default HexColor;
